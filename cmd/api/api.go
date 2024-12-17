900ee806370e045c421eab038d3ca6793a22874a
mubashshir <abduganiyevmubashshir3767@gmail.com>
2024-12-18 00:37:22 +0500

    initial

---
+++ cmd/api/api.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"database/sql"
+	"ecom/service/user"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
+
+func (s *APIServer) handleUsers(w http.ResponseWriter, r *http.Request) {
+	// Example response
+	w.Write([]byte("List of users"))
+}
